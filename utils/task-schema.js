3e2f8b4cf01cbe3f2327cb7ed4fe692f87ab2c63
virendraqx <virendra@qxlabai.com>
2024-03-15 15:23:56 +0530

    vfsdvxcsfdv

--- utils/task-schema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('joi');
-
-const taskSchema = {
-    name: Joi.string().min(3).required(),
-    completed: Joi.boolean()
-};
-
-exports.validateTask = (task) => Joi.validate(task, taskSchema);
